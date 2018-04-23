5d2f33a29ad365f2b73a9216a58539fa3caba183
Joana Durvalo <j.durvalo@gmail.com>
2018-04-23 11:18:41 -0300

    Update config.php

--- frontend/config.php
+++ frontend/config.php
@@ -1,16 +1,15 @@
 <?php
-/* Credenciais de comunicação com o banco. */
+
 define('DB_SERVER', 'backend'); 
 define('DB_USERNAME', 'root'); 
 define('DB_PASSWORD', ''); 
 define('DB_NAME', 'demo');
  
-
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Nāo conectou :( " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
 ?>
